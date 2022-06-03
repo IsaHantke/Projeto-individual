df8cada667164f85a7cedfd621e8b70b08835a1b
IsaHantke <isabela.tenorio@sptech.school>
2022-06-03 15:39:16 -0300

    grafico e api

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,6 +65,8 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var rota = req.body.rotaServer;
+    var confsenha = req.body.confirmacaoSenhaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,10 +75,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    }  else if (rota == undefined) {
+        res.status(400).send("Seu campeao está undefined!");
+    } else if (confsenha == undefined) {
+        res.status(400).send("Sua confsenha está undefined!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, rota, confsenha)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -14,14 +14,10 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     where fk_aquario = ${idAquario}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `select rota.nome, count(usuario.idlogin) as qtdvotos 
+        from rota 
+        join Usuario 
+        on fkrota=idrota group by rota.nome;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -45,13 +41,10 @@ function buscarMedidasEmTempoReal(idAquario) {
                     order by id desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        instrucaoSql = `select rota.nome, count(usuario.idlogin) as qtdvotos 
+        from rota 
+        join Usuario 
+        on fkrota=idrota group by rota.nome;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
